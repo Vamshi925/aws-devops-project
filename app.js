42b2c07c44f55449511da0506ca1c9764ce36c74
Vamshi925 <kvamshireddy5@gmail.com>
2026-03-07 07:17:36 +0000

    Added rds database

--- app.js
+++ app.js
@@ -15,3 +15,20 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const mysql = require("mysql2")
+
+const connection = mysql.createConnection({
+  host: "devops-db.clws0gy0kg8i.ap-south-1.rds.amazonaws.com",
+  user: "admin",
+  password: "StrongPassword123",
+  database: "devopsdb"
+})
+
+connection.connect(err => {
+  if (err) {
+    console.log("DB connection failed")
+  } else {
+    console.log("Connected to RDS")
+  }
+})
